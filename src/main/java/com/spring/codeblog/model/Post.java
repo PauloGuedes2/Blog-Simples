c0cd7d8c51a006b44b63d3821f7ece082e88ffd4
Paulo Guedes <ph.guedes99@outlook.com>
2020-11-30 18:11:28 -0300

    Inserindo mensagem de validação no Form (ainda n está funcionando) e SummerNote para formatação

--- src/main/java/com/spring/codeblog/model/Post.java
+++ src/main/java/com/spring/codeblog/model/Post.java
@@ -17,16 +17,16 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Getter private Long id;
 
-    @NotBlank
+    @NotBlank(message="O campo nome é obrigatório!")
     @Getter @Setter private String titulo;
 
-    @NotBlank
+    @NotBlank(message="O campo autor é obrigatório!")
     @Getter @Setter private String autor;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
     @Getter @Setter private LocalDate data;
 
-    @NotBlank
+    @NotBlank(message="Por favor, digite algum texto!")
     @Lob
     @Getter @Setter private String texto;
 }
